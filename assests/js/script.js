d7d9286dd5741e28f3e02275f8013cf48c68254c
rajiv.gupta <rajiv.gupta@acldigital.com>
2024-08-08 18:51:23 +0530

    Changes made for mobile responsiveness!

--- assests/js/script.js
+++ assests/js/script.js
@@ -53,7 +53,7 @@ function(){
 
 // <!-- typed js effect starts -->
     var typed = new Typed(".typing-text", {
-        strings: ["Front-End Developer", "Web Designing"],
+        strings: ["Full-Stack Developer", "Web Designing"],
         loop: true,
         typeSpeed: 50,
 		backSpeed: 25,
